2e151bfb518c3b8d70e6e779b40b6e1a9cf50565
DESKTOP-G0KHUU1\JRemonte <june.remonte@taxsystems.com>
2022-09-08 14:02:07 +0800

    first commit

---
+++ CRUD-API-Automation-RestSharp/CRUD-API-Automation-RestSharp/SetupTeardown.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace CRUD_API_Automation_RestSharp
+{
+    public class SetupTeardown
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+        }
+    }
+}
